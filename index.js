8f9d7e52603d923b7cf6495740a7063635126aec
unknown <mgznlidia2@gmail.com>
2026-05-05 21:27:34 +0300

    some minor changes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const Button = document.getElementById("playButton");
+const BackButton = document.getElementById("getBackButton");
+const ForwardButton = document.getElementById("getForwardButton");
+const RemoveButton = document.getElementById("removeButton");
+
+const ClearHistoryButton = document.getElementById("clearHistoryButton");
+
+
 const linksArea = document.getElementById("linksArea");
-Button.onclick = () => {
+if(!localStorage.getItem('prevIndex')) localStorage.setItem('prevIndex', "0");
+if(!localStorage.getItem('length')) localStorage.setItem('length', "0");
+
+if(localStorage.getItem(localStorage.getItem('prevIndex'))) linksArea.value = JSON.parse(localStorage.getItem(localStorage.getItem('prevIndex')));
     
+
+Button.onclick = () => {
+    if(!localStorage.getItem('prevIndex')) localStorage.setItem('prevIndex', "0");
+    let prevIndex = JSON.parse(localStorage.getItem('prevIndex'));
+    let historyLength = JSON.parse(localStorage.getItem('length'));
     // window.open("https://cash-calc.org", '_blank');  
     // window.open("https://youtube.com", '_blank');   
 
-    console.log(linksArea.value);
     const linksUnparsed = linksArea.value;
     const linksArray = linksUnparsed.split('\n');
     for(link of linksArray){
@@ -13,7 +27,52 @@ Button.onclick = () => {
            window.open(link, '_blank');   
         }
     }
+    historyLength++;
+    localStorage.setItem('length', historyLength);
+    localStorage.setItem("prevIndex", JSON.stringify(prevIndex + 1));
+    localStorage.setItem(JSON.stringify(prevIndex + 1), JSON.stringify(linksUnparsed));
 }
+
+BackButton.onclick = () => {
+    if(!localStorage.getItem('prevIndex')) return;
+    let prevIndex = JSON.parse(localStorage.getItem('prevIndex'));
+    let currentIndex = prevIndex - 1;
+    console.log(prevIndex);
+    if(!localStorage.getItem(JSON.stringify(currentIndex)))
+        return;
+    linksArea.value = JSON.parse(localStorage.getItem(JSON.stringify(currentIndex)));
+    localStorage.setItem("prevIndex", JSON.stringify(currentIndex));
+}
+
+ForwardButton.onclick = () => {
+    if(!localStorage.getItem('prevIndex')) return;
+    let prevIndex = JSON.parse(localStorage.getItem('prevIndex'));
+    let currentIndex = prevIndex + 1;
+    console.log(prevIndex);
+    if(!localStorage.getItem(JSON.stringify(currentIndex))) return;
+    linksArea.value = JSON.parse(localStorage.getItem(JSON.stringify(currentIndex)));
+    localStorage.setItem("prevIndex", JSON.stringify(currentIndex));
+}
+
+RemoveButton.onclick = () => {
+    let prevIndex = JSON.parse(localStorage.getItem('prevIndex'));
+    let getData = JSON.parse(localStorage.getItem(prevIndex - 1));
+    linksArea.value = getData;
+    let historyLength = JSON.parse(localStorage.getItem('length'));
+
+    localStorage.removeItem(prevIndex);
+    for(let i = prevIndex; historyLength > i; i++){
+        let dataHolder = localStorage.getItem(i + 1);
+        localStorage.setItem(i, dataHolder);
+    }
+    localStorage.setItem("length", historyLength - 1);
+    localStorage.setItem("prevIndex", prevIndex-1);
+}
+
+ClearHistoryButton.onclick = () => {
+    localStorage.clear();
+}
+
 
 linksArea.addEventListener('paste', (event)=>{
     setTimeout(() => {
